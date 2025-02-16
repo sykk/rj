95a857920add093fde8e1079352abff288284722
syk <sykk@aol.com>
2025-02-16 14:48:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const token = process.env.DISCORD_TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
 
 client.commands = new Collection();
 
 const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
+const commands = [];
 for (const file of commandFiles) {
     const command = require(path.join(__dirname, 'commands', file));
     client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
 }
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
 client.once('ready', () => {
     console.log('Bot is ready');
