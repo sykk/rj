c3fa7367edb2b2c9f28393f6e3e3733f5e3b3e35
syk <sykk@aol.com>
2025-02-16 11:06:41 -0500

    Update watcher.js

--- watcher.js
+++ watcher.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
