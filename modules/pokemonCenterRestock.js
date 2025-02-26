8afdb354d74982ec5eedf583ce55ac02501b09b7
syk <sykk@aol.com>
2025-02-26 16:19:52 -0500

    f

--- modules/pokemonCenterRestock.js
+++ modules/pokemonCenterRestock.js
@@ -1,76 +1,17 @@
-import fetch from 'node-fetch';
-import * as cheerio from 'cheerio';
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const { sendWebhook } = require('../utils/webhook');
 
+const knownProducts = new Map();
 const urls = [
-    'https://www.pokemoncenter-online.com/?p_cd=4521329421353',
-'https://www.pokemoncenter-online.com/?p_cd=4521329421377'
+    'https://www.pokemoncenter.com',
+    // Add other URLs here
 ];
-const webhookLink = "https://discord.com/api/webhooks/1344414560869810218/qkp-UMK98pZLrgsIBCzb9miFOZXft9NTtPIUTSMkTaenTdhy95ETm1r95yfQSqE8Ci1J";
-let knownProducts = new Map();
-
-function sendWebhook(title, article, link, image, price, stockStatus) {
-    try {
-        fetch(
-            webhookLink,
-            {
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: 'Pokemon Center JP Monitor',
-                    avatar_url: 'https://affluencestore.fr/wp-content/uploads/2024/03/LogoBackWhite.png',
-                    embeds: [
-                        {
-                            color: 5763719,
-                            title: title,
-                            url: link ? link : 'https://keinemusik.com',
-                            thumbnail: {
-                                url: image ? image : 'https://affluencestore.fr/wp-content/uploads/2024/03/LogoBackWhite.png'
-                            },
-                            footer: {
-                                text: 'Affluence Monitor',
-                                icon_url: 'https://affluencestore.fr/wp-content/uploads/2024/03/LogoBackWhite.png',
-                            },
-                            fields: article ? [
-                                {
-                                    name: 'Article',
-                                    value: article,
-                                    inline: true
-                                },
-                                {
-                                    name: 'Price',
-                                    value: price,
-                                    inline: true
-                                },
-                                {
-                                    name: 'Stock Status',
-                                    value: stockStatus,
-                                    inline: true
-                                },
-                            ] : [],
-                        },
-                    ],
-                }),
-            }
-        );
-    } catch (error) {
-        console.log(error)
-    }
-}
 
 async function fetchDataPokemonCenter(isInitial = false) {
     try {
         for (const url of urls) {
-            let response;
-            for (let attempt = 0; attempt < 3; attempt++) {
-                try {
-                    response = await fetch(url, { timeout: 10000 }); // 10 seconds timeout
-                    if (response.ok) break;
-                } catch (error) {
-                    if (attempt === 2) throw error; // Throw error after 3 attempts
-                }
-            }
+            const response = await fetch(url);
             const body = await response.text();
             const $ = cheerio.load(body);
 
@@ -133,9 +74,14 @@ function automaticGetStock() {
 }
 
 // Initial fetch to populate known products
-fetchDataPokemonCenter(true);
-automaticGetStock();
+fetchDataPokemonCenter(true).then(() => {
+    console.log('Initial product list fetched. Starting automatic checks...');
+    sendWebhook("Pokemon Center JP Monitor Started", "", "", "", "");
+    automaticGetStock();
+});
 
-export const data = {
+module.exports = {
+    data: {
         name: 'pokemonCenterRestock'
-     };
+    }
+};
